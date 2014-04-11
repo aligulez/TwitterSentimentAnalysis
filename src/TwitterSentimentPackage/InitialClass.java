2ba5acd524f780ed0dc46debf6d1c0710c807adf
Ali Gulez <aligulez@hotmail.com>
2014-04-11 17:45:41 +0300

    Initial start
    hello world

---
+++ src/TwitterSentimentPackage/InitialClass.java
@@ -0,0 +1,12 @@
+package TwitterSentimentPackage;
+
+public class InitialClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Deneme") ;
+		
+	}
+
+}
