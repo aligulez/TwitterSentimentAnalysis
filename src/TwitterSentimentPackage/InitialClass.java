a0d8ca4a45f20ec6be915789cd0eede5bfa476a3
Ali Gulez <aligulez@hotmail.com>
2014-04-11 21:44:56 +0300

    extracting fields from tweets
    remove: usernames, links, repetitive characters (huuungry)
    read: unigrams, bigrams.
    keep: emoticons.

--- src/TwitterSentimentPackage/InitialClass.java
+++ src/TwitterSentimentPackage/InitialClass.java
@@ -1,11 +1,66 @@
 package TwitterSentimentPackage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.JsonGenerationException ;
+
+
+
+
+
 public class InitialClass {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		    JsonFactory f = new JsonFactory();
 
-		System.out.println("Deneme") ;
+		    try {
+				
+		    	File file = new File("/Users/aligulez/Desktop/IRDM-Data/exampletweets_2.txt") ;
+		    	
+		    	InputStream inputStream = new FileInputStream(file) ;
+		    	
+		    	JsonParser p = f.createJsonParser(inputStream) ;
+		    	
+		    	
+		    	  while (p.nextToken() != JsonToken.END_OBJECT) { //loop through all the fields 
+				    	
+		    		  String fieldname = p.getCurrentName();
+		    		  
+		    		  String value = p.getValueAsString() ;
+		    		  
+		    		  System.out.println(fieldname) ;
+		    		  System.out.println(value) ;
+
+		    		  
+
+				  }//end while 
+				    
+		    	  p.close() ;//close the parser
+		    	  
+		    	  
+		    	  
+		    	  
+		    	  
+		    	  
+		    	  
+			// CATCH statements 
+		    } catch (JsonParseException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		    
+		  
+		System.out.println("Hello World") ;
 		
 	}
 
