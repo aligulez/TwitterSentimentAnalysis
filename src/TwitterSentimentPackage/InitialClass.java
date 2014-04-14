ec5dc5964d5bfef0151652f80725a5ab5c1904a7
Ali Gulez <aligulez@hotmail.com>
2014-04-14 17:37:05 +0300

    Resolved; -double hashtag text bug -RT repetitive printing bug
    Todo;
    
    -pick only english
    -additional fields add
         1) created_at : So that we can do analysis for all those tweets
    that have been created in the same month 2) favorite_count
    3)retweet_count 4)user has followers_count
    -repetitive words
    -no punctuations
    -count bigrams /AWS?

--- src/TwitterSentimentPackage/InitialClass.java
+++ src/TwitterSentimentPackage/InitialClass.java
@@ -1,10 +1,12 @@
 package TwitterSentimentPackage;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
@@ -12,7 +14,6 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
-import com.fasterxml.jackson.core.JsonGenerationException ;
 
 public class InitialClass {
 
@@ -20,19 +21,20 @@ public class InitialClass {
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		
+
 		    JsonFactory f = new JsonFactory();
-		    String newTweet ;
 		    
 		    try {
 				//initialise file, reader and parser.
-		    	File file = new File("/Users/aligulez/Desktop/IRDM-Data/test.txt") ;
+		    	File file = new File("/Users/aligulez/Desktop/IRDMGroup/test.txt") ;
 		    	InputStream inputStream = new FileInputStream(file) ;		    	
 		    	JsonParser p = f.createJsonParser(inputStream) ;// p is the parser
-		    	JsonGenerator g = f.createJsonGenerator(new File("/Users/aligulez/Desktop/IRDM-Data/output_data.json"), JsonEncoding.UTF8);// g is the generator
+		    	JsonGenerator g = f.createJsonGenerator(new File("/Users/aligulez/Desktop/IRDMGroup/output_data.json"), JsonEncoding.UTF8);// g is the generator
 		    	
 		    	JsonToken current; 
 		    	
 		    	current = p.nextToken();
+
 		    	
 		    	//check if initial object is a start of the tweet object
 		    	if (current != JsonToken.START_OBJECT){
@@ -47,12 +49,26 @@ public class InitialClass {
 		    		current = p.nextToken() ; 
 		    		String fieldname = p.getCurrentName() ;
 		    		
-		    		if(current == JsonToken.START_OBJECT && fieldname != null ){//detect internal objects within tweets
+				    String newTweet ;
+
+		    		
+		    		//skip entities since dont want to print "text" field of hashtags. 
+		    		if ("entities".equals(fieldname)){
 		    			
-		    			while(p.nextToken() != JsonToken.END_OBJECT){
-		    				  //do nothing 
-		    			}//end while
-		    		}//end internal object skip
+		    			while(!("favorited".equals(fieldname))){
+		    				current = p.nextToken() ;
+		    				fieldname = p.getCurrentName();
+		    			}//end while		    			
+		    		}//end if 
+		    		
+		    		//skip retweeted_statys since dont want to print repetitive "text" fields. 
+		    		if ("retweeted_status".equals(fieldname)){
+		    			
+		    			while(!("geo".equals(fieldname))){
+		    				current = p.nextToken() ;
+		    				fieldname = p.getCurrentName();
+		    			}//end while		    			
+		    		}//end if 
 		    		
 		    		if ("text".equals(fieldname)) {//detect text fieldname within tweet
 
@@ -67,10 +83,23 @@ public class InitialClass {
 		    			while(itr.hasMoreTokens()) {//iterate over words in the tweet 
 		    						    				
 		    				String word = itr.nextToken() ;// each word in a single tweet
-		    	
-		    				if(word.charAt(0) == '@'){
+		    				
+		    				//Detect Web Links contained within "text"
+		    			    Pattern patLink1 = Pattern.compile("http:"); 
+		    			    Matcher matLink1 = patLink1.matcher(word) ;
+		    			    Pattern patLink2 = Pattern.compile("https:"); 
+		    			    Matcher matLink2 = patLink2.matcher(word) ;
+		    			    
+		    			    //detect and remove punctuation within "text"
+		    			    //Pattern patPunc = Pattern.compile("[][(){},.;!?<>%") ;
+		    			    //patPunc.matcher(word).replaceAll("");
+		    			    
+		    				if(word.charAt(0) == '@'){//for removing usernames 
 		    					//do nothing
-		    				}else{
+		    				} else if(matLink1.find() || matLink2.find()){ 		    			    
+		    					//do nothing 
+		    					//System.out.println(mat2.group(0));
+		    				}else{						
 		    						
 		    					if (newTweet.equals("")){//for the initial word 
 		    						newTweet = word; 		    		
@@ -82,15 +111,14 @@ public class InitialClass {
 		    				}//end if
 		    			
 		    			}//end while	
-		    			
+		    		
 		    			g.writeStartObject(); 
 		    			g.writeStringField("text", newTweet);
 		    			g.writeEndObject();
 		    			System.out.println(newTweet) ;
 		    			
-		    			
 		    		 }//end if 
-		    
+		    		
 		    	}//end while 
 		    			
 		    	//will not complete unless closed 
@@ -112,24 +140,19 @@ public class InitialClass {
 
 }
 
+/*
+if ("lang".equals(fieldname)){
 
-
-/*	CONSIDER INTERNAL OBJECTS AND DETECT
-while (p.nextToken() != JsonToken.END_OBJECT) { //loop through all the fields 
-  
-	  String fieldname = p.getCurrentName();
-	  System.out.println(fieldname) ;
-
-	  current = p.nextToken();
-	  
-	  if (current == JsonToken.START_OBJECT){
-		  		    	
-		  while(p.nextToken() != JsonToken.END_OBJECT){
-			  //do nothing 
-		  }
+	current = p.nextToken();
+	
+	if (p.getValueAsString().equals("en")){
 		
-	  }//end internal object checkers 
-	  
-
-}//end tweet object 
+		g.writeStartObject(); 
+		g.writeStringField("text", newTweet);
+		g.writeEndObject();
+		System.out.println(newTweet) ;
+	
+	}//end if 
+	
+}//end if language checker 
 */
