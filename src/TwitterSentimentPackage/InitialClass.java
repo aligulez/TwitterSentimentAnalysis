87012b364d8daab85c2b9ccf2458f7698ba1ea03
Ali Gulez <aligulez@hotmail.com>
2014-04-15 16:07:49 +0300

    AWS preprocess Final
    Resolved;
    - Easy switch between write to JSON and .TXT(for AWS)
    - Repetition bug
    - AWS Count Bigrams and Unigrams + Analysis
    
    Todo ;
    - add additional fields.

--- src/TwitterSentimentPackage/InitialClass.java
+++ src/TwitterSentimentPackage/InitialClass.java
@@ -1,6 +1,7 @@
 package TwitterSentimentPackage;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.StringTokenizer;
@@ -25,16 +26,20 @@ public class InitialClass {
 		    
 		    try {
 				//initialize file, reader and parser.
-		    	File file = new File("/Users/aligulez/Desktop/IRDMGroup/test.txt") ;
+		    	File file = new File("/Users/aligulez/Desktop/IRDMGroup/exampletweets_2.txt") ;
 		    	InputStream inputStream = new FileInputStream(file) ;		    	
 		    	JsonParser p = f.createJsonParser(inputStream) ;// p is the parser
 		    	JsonGenerator g = f.createJsonGenerator(new File("/Users/aligulez/Desktop/IRDMGroup/output_data.json"), JsonEncoding.UTF8);// g is the generator
-		    	
+		    	//UNCOMMENT BELOW FOR WRITING TO TEXT FILE
+		    	//FileWriter f0 = new FileWriter("/Users/aligulez/Desktop/IRDMGroup/output.txt");
+		    	//String newLine = System.getProperty("line.separator");
+
 		    	JsonToken current; 
 		    	
 		    	current = p.nextToken();
 		    	
-		    	String newTwt = new String() ;
+		    	String newTwt = new String() ;//for writing the pre-processed tweet 
+		    	 
 		    	
 		    	//check if initial object is a start of the tweet object
 		    	if (current != JsonToken.START_OBJECT){
@@ -43,18 +48,23 @@ public class InitialClass {
 		    		current = p.nextToken();//move to next token. 
 		    		
 		    	}//end if
-	    			    	
-		    	int counter = 0 ;
-		    	
+	    			    			    	
 		    	while(p.hasCurrentToken()== true){
-		    		
+		    				    		
 		    		current = p.nextToken() ; 
-		    		String fieldname = p.getCurrentName() ;
-		    						 
+		    		String fieldname = p.getCurrentName() ;			    
+		    				    		
 		    		
 		    		//pre-processing the "text"
-		    		if ("text".equals(fieldname)) {//detect text fieldname within tweet
-
+		    		if ("id_str".equals(fieldname)) {//First recognize "id_str" for recognizing the right "text" field.
+		    			
+		    			while(!("text".equals(fieldname))){
+		    				current = p.nextToken() ;
+		    				fieldname = p.getCurrentName();
+		    				
+		    			}//end while
+		    			
+		    			if("text".equals(fieldname)){//detect text fieldname within tweet
 		    		
 		    			current = p.nextToken();
 
@@ -74,18 +84,13 @@ public class InitialClass {
 		    			    Pattern patLink2 = Pattern.compile("https:"); 
 		    			    Matcher matLink2 = patLink2.matcher(word) ;
 		    			    
-		    			    //detect and remove punctuation within "text"
-		    			    //Pattern patPunc = Pattern.compile("[\\Q][(){},.;!?<>%\\E]") ;//   [][(){},.;!?<>%
-		    			    //word = patPunc.matcher(word).replaceAll("");
-		    			   
-		    			    
 		    				if(word.charAt(0) == '@'){//for removing usernames 
 		    					//do nothing
 		    				} else if(matLink1.find() || matLink2.find()){ 		    			    
 		    					//do nothing 
-		    					//System.out.println(mat2.group(0));
 		    				}else{						
 		    					
+		    					//detect and remove punctuation within "text"			    			   
 		    					word = word.replaceAll("[\\Q][(){},.:;!?<>%\\E]", "") ;
 		    					
 		    					if (newTwt.equals("")){//for the initial word 
@@ -101,38 +106,41 @@ public class InitialClass {
 		    		
 		    			//skip all the fields until reaching "lang"
 		    			while(!("filter_level".equals(fieldname))){
-		    				current = p.nextToken() ;
-		    				fieldname = p.getCurrentName();
 		    				
-		    			}
+		    				if(p.hasCurrentToken() == true){
+		    				current = p.nextToken() ;
+		    				fieldname = p.getCurrentName(); } else {break ;}
+		    				
+		    			}//end while
 		    			
-		    			
-		    		 }//end if 
-		    		
+		    		}//end internal if	
+		    	 }//end if 
+
+		    		//detect tweet language, only print if its in English. 
 		    		if ("lang".equals(fieldname)){
 
 		    			current = p.nextToken();
 		    			
-		    			if (p.getValueAsString().equals("en")){
-		    				
-		    				//counter = counter + 1 ; 
+		    			String Valyu = p.getValueAsString() ;
+		    			
+		    			if (Valyu.equals("en")){
+		    						    				
+		    				//f0.write(newTwt + newLine); UNCOMMENT FOR WRITING TO TEXT FILE
 		    				g.writeStartObject(); 
 		    				g.writeStringField("text", newTwt);
 		    				g.writeEndObject();
 		    				System.out.println(newTwt) ;
-		    				newTwt = "" ;
-		    				
-		    			
+		    				newTwt = "" ;		    						    			
 		    			}//end if 		 
-		    		
-		    		
+		    			
 		    		}//end if language checker 
 	    		
-		    		//if (counter == 300){ break ;}
-		    		
+		    				
 		    	}//end while 
-		    			
+		    	
+		    
 		    	//will not complete unless closed 
+		    	//f0.close(); UNCOMMENT FOR WRITING TO TEXT FILE
 		    	g.close(); // close generator 
 		    	p.close() ;//close the parser
 			
@@ -144,26 +152,8 @@ public class InitialClass {
 				e.printStackTrace();
 			}
 		    
-		  
 		System.out.println("Hello World") ;
 		
 	}
 
 }
-
-/*
-if ("lang".equals(fieldname)){
-
-	current = p.nextToken();
-	
-	if (p.getValueAsString().equals("en")){
-		
-		g.writeStartObject(); 
-		g.writeStringField("text", newTweet);
-		g.writeEndObject();
-		System.out.println(newTweet) ;
-	
-	}//end if 
-	
-}//end if language checker 
-*/
